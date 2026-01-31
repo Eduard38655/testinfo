88299c07e4df48dc026939bb9ce84ceb603ecfe0
Eduardo <eduao154@gmail.com>
2026-01-31 17:40:46 -0400

    Update vite.config.js

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -3,12 +3,13 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+     base:"/testinfo/",
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-       base:"/testinfo/"
+     
     }),
   ],
 })
